f02b80f9ea9fc52e92e8d61f160d22340690756b
Miguelangel <mig2k12@outlook.es>
2026-02-13 18:29:02 +0000

    Añadir quienes somos y css de botones

--- src/front/pages/Home.jsx
+++ src/front/pages/Home.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import rigoImageUrl from "../assets/img/rigo-baby.jpg";
 import useGlobalReducer from "../hooks/useGlobalReducer.jsx";
 import SkillBankBanner  from "../components/SkillBankBanner.jsx";
+import SkillBankAbout from "../components/SkillBankAbout.jsx"
 
 
 import InfoCard from "../components/InfoCard.jsx";
@@ -38,6 +39,7 @@ export const Home = () => {
   return (
     <div>
         <SkillBankBanner />
+        <SkillBankAbout />
     </div>
   );
 };
